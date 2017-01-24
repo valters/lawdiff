fd2c2c64135a4f0e2f11af22e45111452fae7b0c
Valters Vingolds <valters@vingolds.ch>
2017-01-24 14:03:27 +0100

    work around maven complaint about missing sources/javadoc

--- LawDiff-app/src/main/java/io/github/valters/lawdiff/app/NoSourcesYet.java
+++ LawDiff-app/src/main/java/io/github/valters/lawdiff/app/NoSourcesYet.java
@@ -1,5 +1,5 @@
 package io.github.valters.lawdiff.app;
 
 /** make sure sources/javadoc artifact get generated. remove when actual sources are added */
-abstract class NoSourcesYet {
+public final class NoSourcesYet {
 }
